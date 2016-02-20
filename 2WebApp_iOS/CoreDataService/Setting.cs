a06b6d950b794a57c4b8d818b8f04d714fb74105
yix785 <yix785@mail.usask.ca>
2016-02-20 13:09:38 -0600

    CoreDataService delivery
    CoreDataService provides service retrieving data from local and remote
    database.

---
+++ 2WebApp_iOS/CoreDataService/Setting.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+
+namespace CoreDataService
+{
+	
+	// callback function definition
+	// Parameters:
+	// succeed		true | false
+	// errmsg		error message
+	delegate void SyncCallback(Boolean succeed, string errmsg);
+	
+	
+	public static class Settings
+	{
+		// remote settings
+		public static string ws_address = "http://www.2web.cc.php53-4.ord1-1.websitetestlink.com";
+		public static string ws_basepath = "/webservice/common/";
+		public static string ws_username = "";
+		public static string ws_password = "";
+		public static Boolean ws_SyncRequest = true;
+
+		// local settings
+		public static string local_dbpath = "2webdesign.sqlite";
+		public static string local_dbschema = "CoreDataService.";
+		public static string[] local_tables = { "organization","staffaccount","taskupdatetypes","notification_type","notifications",
+			"client_organization_rel","projects","tasks","project_support_rel","clientaccount",
+			"projectstatus","projecttype","projectphase","supportpackage"};
+
+		// global functions
+		public static void ShowMsg(string msg) {
+			UIAlertView msgview = new UIAlertView ("Notification", msg, null, "Ok", null);
+			msgview.Show ();
+		}
+		
+		public static void CallbackBody(Boolean succeed, string errmsg) 
+		{
+
+		}
+	}
+}
+
