da8fbaf274924ccbddf795eb9b3f4e44f7b41f52
yix785 <yix785@mail.usask.ca>
2016-03-01 21:46:36 -0600

    Backend: Create default project and contact information 	1) Add a default project and contact information for offline showcase

--- 2WebApp_iOS/CoreDataService/Setting.cs
+++ 2WebApp_iOS/CoreDataService/Setting.cs
@@ -35,41 +35,93 @@ namespace CoreDataService
 			"userinfo"};
 
 
+		// Return default contact
+		public static contact DefaultContact() {
+
+			contact info = new contact();
+			info.address1 = "116-116 Research Drive, Saskatoon,";
+			info.address2 = "SK S7N3R3";
+			info.email = "info@2webdesign.com";
+			info.phone = "306.664.2932";
+			info.visit = "www.2webdesign.com";
+			info.facebook = "https://www.facebook.com/2webdesign";
+			info.twitter = "https://twitter.com/2webdesign";
+			info.google = "https://plus.google.com/u/0/+2webdesign/posts";
+			info.linkedIn = "https://www.linkedin.com/company/2webdesign-com";
+			info.youtube = "https://www.pinterest.com/2webdesign";
+
+			return info;
+		}
+		
+		
 		// Return the demo project
 		public static projectsummary DemoProject() {
 
 			projectsummary demo = new projectsummary();
-			demo.name = "";
-			demo.type = "";
-			demo.status = "";
-			demo.org_name = "";
-			demo.client_name = "";
-			demo.client_email = "";
-			demo.staff_name = "";
-			demo.staff_email = "";
-
-			task newtask = new task ();
-			newtask.name = "";
-			newtask.date = "";
-			newtask.file_url = "";
-
-			support newsupp = new support ();
-			newsupp.name = "";
-			newsupp.status = "";
-			newsupp.hourused = "";
-			newsupp.totalhour = "";
-			newsupp.lastbackup = "";
-			newsupp.lastpost = "";
+			demo.name = "2 Web Demo Project";
+			demo.type = "DEMO";
+			demo.status = "In Process";
+			demo.org_name = "2 Web Design Demo";
+			demo.client_name = "0";
+			demo.client_email = "test@test.com";
+			demo.staff_name = "0";
+			demo.staff_email = "hardik@2webdesign.com";
 
 			demo.update =new List<task> ();
+			task newtask = new task ();
+			newtask.name = "Project Proposal delivered to client";
 			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Client Agreement Signed";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Project Charter delivered";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Project Charter Signed";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Homepage Designs started";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Internal Page designs started";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Designs sent to client for feedback";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Design Review Meeting";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Design Approval Signed";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Database setup complete";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Navigation Design Change Requested";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "Slice Updated for Navigation Change";
+			demo.update.Add (newtask);
+			newtask = new task ();
+			newtask.name = "New Slice for Navigation change applied";
+			demo.update.Add (newtask);
+			
 			demo.support_package = new List<support> ();
+			support newsupp = new support ();
+			newsupp.name = "360 Website Success Plan A";
+			newsupp.status = "1";
+			newsupp.hourused = "3";
+			newsupp.totalhour = "12";
+			newsupp.lastbackup = "2016-02-01 00:00:00";
+			newsupp.lastpost = "2016-03-01 00:00:00";
 			demo.support_package.Add (newsupp);
 
 			return demo;
 		}
 
-
+		
 		// global functions
 		public static void ShowMsg(string msg) {
 			UIAlertView msgview = new UIAlertView ("Notification", msg, null, "Ok", null);
