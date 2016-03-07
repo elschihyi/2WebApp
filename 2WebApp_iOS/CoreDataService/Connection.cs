7ef69631914321f6b53a9162de8902560b3213ad
yix785 <yix785@mail.usask.ca>
2016-03-07 17:59:48 -0600

    Backend: Network Performance Improvement
    	Connection
    	1) Use HTTP compression as possible by Accept-Encoding setting
    	2) Update CheckConnection() by checking the Ping service on the Web server
    
    	Web Server
    	3) Add a PIng service for connection detections

--- 2WebApp_iOS/CoreDataService/Connection.cs
+++ 2WebApp_iOS/CoreDataService/Connection.cs
@@ -4,8 +4,7 @@ using RestSharp;
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
-using SystemConfiguration;
-using CoreFoundation;
+
 
 namespace CoreDataService
 {
@@ -14,14 +13,19 @@ namespace CoreDataService
 
 		// check if connection is available and accessible
 		protected Boolean CheckConnection(out string errmsg) {
-
+			
 			errmsg = "";
-			NetworkStatus internetStatus = Reachability.InternetConnectionStatus();
-			if ( internetStatus == NetworkStatus.NotReachable ) {
-				errmsg = "Internet connection is not available";
-				return false;
-			} else if ( !Reachability.IsHostReachable(Settings.ws_address) ) {
-				errmsg = "The web service can not be reached";
+			string magicToken = Path.GetRandomFileName();
+			
+			Dictionary<string, string> request = new Dictionary<string, string> ();
+			request.Add ("ADDRESS", Settings.ws_address);
+			request.Add ("PATH", Settings.ws_basepath + "Ping");
+			request.Add ("FORMAT", "json");
+			request.Add ("BODY",magicToken);
+			
+			string data;
+			if ( !MakeRequest( request, out data) || data == null || data != magicToken ) {
+				errmsg = "Connection is not available";
 				return false;
 			}
 			
@@ -80,13 +84,13 @@ namespace CoreDataService
 					else if (Settings.ws_reqtype == RequestType.POST ) {
 						req = new RestRequest (request["PATH"], Method.POST);
 						req.AddParameter("application/json; charset=utf-8", request["BODY"], ParameterType.RequestBody);
-						req.RequestFormat = DataFormat.Json;
 					}
 					else {
 						data = "The given request type is not supported";
 						return false;
 					}
-					
+
+					req.AddHeader("Accept-Encoding","gzip,deflate");
 					req.AddHeader("Accept", "application/json");
 					req.Timeout = Settings.ws_timeout;
 				}
@@ -114,143 +118,5 @@ namespace CoreDataService
 		}
 
 	}
-	
-	
-	
-	
-	
-	public enum NetworkStatus {
-		NotReachable,
-		ReachableViaCarrierDataNetwork,
-		ReachableViaWiFiNetwork
-	}
 
-	public static class Reachability
-	{
-		public static string HostName = Settings.ws_address;
-
-		public static bool IsReachableWithoutRequiringConnection (NetworkReachabilityFlags flags)
-		{
-			// Is it reachable with the current network configuration?
-			bool isReachable = (flags & NetworkReachabilityFlags.Reachable) != 0;
-
-			// Do we need a connection to reach it?
-			bool noConnectionRequired = (flags & NetworkReachabilityFlags.ConnectionRequired) == 0
-				|| (flags & NetworkReachabilityFlags.IsWWAN) != 0;
-
-			return isReachable && noConnectionRequired;
-		}
-
-		// Is the host reachable with the current network configuration
-		public static bool IsHostReachable(string host)
-		{
-			if (string.IsNullOrEmpty(host))
-				return false;
-
-			using (var r = new NetworkReachability(host)) {
-				NetworkReachabilityFlags flags;
-
-				if (r.TryGetFlags(out flags))
-					return IsReachableWithoutRequiringConnection(flags);
-			}
-			return false;
-		}
-
-		//
-		// Raised every time there is an interesting reachable event,
-		// we do not even pass the info as to what changed, and
-		// we lump all three status we probe into one
-		//
-		public static event EventHandler ReachabilityChanged;
-
-		static void OnChange(NetworkReachabilityFlags flags)
-		{
-			var h = ReachabilityChanged;
-			if (h != null)
-				h(null, EventArgs.Empty);
-		}
-
-		//
-		// Returns true if it is possible to reach the AdHoc WiFi network
-		// and optionally provides extra network reachability flags as the
-		// out parameter
-		//
-		static NetworkReachability adHocWiFiNetworkReachability;
-
-		public static bool IsAdHocWiFiNetworkAvailable (out NetworkReachabilityFlags flags)
-		{
-			if (adHocWiFiNetworkReachability == null) {
-				adHocWiFiNetworkReachability = new NetworkReachability(new IPAddress(new byte [] { 169, 254, 0, 0 }));
-				adHocWiFiNetworkReachability.SetNotification(OnChange);
-				adHocWiFiNetworkReachability.Schedule(CFRunLoop.Current, CFRunLoop.ModeDefault);
-			}
-
-			return adHocWiFiNetworkReachability.TryGetFlags(out flags) && IsReachableWithoutRequiringConnection(flags);
-		}
-
-		static NetworkReachability defaultRouteReachability;
-
-		static bool IsNetworkAvailable(out NetworkReachabilityFlags flags)
-		{
-			if (defaultRouteReachability == null) {
-				defaultRouteReachability = new NetworkReachability(new IPAddress(0));
-				defaultRouteReachability.SetNotification(OnChange);
-				defaultRouteReachability.Schedule(CFRunLoop.Current, CFRunLoop.ModeDefault);
-			}
-			return defaultRouteReachability.TryGetFlags(out flags) && IsReachableWithoutRequiringConnection(flags);
-		}
-
-		static NetworkReachability remoteHostReachability;
-
-		public static NetworkStatus RemoteHostStatus()
-		{
-			NetworkReachabilityFlags flags;
-			bool reachable;
-
-			if (remoteHostReachability == null) {
-				remoteHostReachability = new NetworkReachability (HostName);
-
-				// Need to probe before we queue, or we wont get any meaningful values
-				// this only happens when you create NetworkReachability from a hostname
-				reachable = remoteHostReachability.TryGetFlags (out flags);
-
-				remoteHostReachability.SetNotification (OnChange);
-				remoteHostReachability.Schedule (CFRunLoop.Current, CFRunLoop.ModeDefault);
-			} else {
-				reachable = remoteHostReachability.TryGetFlags (out flags);
-			}
-
-			if (!reachable)
-				return NetworkStatus.NotReachable;
-
-			if (!IsReachableWithoutRequiringConnection(flags))
-				return NetworkStatus.NotReachable;
-
-			return (flags & NetworkReachabilityFlags.IsWWAN) != 0 ?
-				NetworkStatus.ReachableViaCarrierDataNetwork : NetworkStatus.ReachableViaWiFiNetwork;
-		}
-
-		public static NetworkStatus InternetConnectionStatus ()
-		{
-			NetworkReachabilityFlags flags;
-			bool defaultNetworkAvailable = IsNetworkAvailable(out flags);
-			if (defaultNetworkAvailable && ((flags & NetworkReachabilityFlags.IsDirect) != 0))
-				return NetworkStatus.NotReachable;
-			else if ((flags & NetworkReachabilityFlags.IsWWAN) != 0)
-				return NetworkStatus.ReachableViaCarrierDataNetwork;
-			else if (flags == 0)
-				return NetworkStatus.NotReachable;
-			return NetworkStatus.ReachableViaWiFiNetwork;
-		}
-
-		public static NetworkStatus LocalWifiConnectionStatus()
-		{
-			NetworkReachabilityFlags flags;
-			if (IsAdHocWiFiNetworkAvailable(out flags))
-			if ((flags & NetworkReachabilityFlags.IsDirect) != 0)
-				return NetworkStatus.ReachableViaWiFiNetwork;
-
-			return NetworkStatus.NotReachable;
-		}
-	}
 }
